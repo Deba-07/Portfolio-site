72dd883a933291a6a224d993078f13b946135395
Deba-07 <dynamicdeba07@gmail.com>
2024-10-21 23:04:46 +0530

    comments removed

--- Protfolio/src/components/About.jsx
+++ Protfolio/src/components/About.jsx
@@ -1,35 +1,11 @@
-/**
- * About component
- *
- * Space for you to describe more about yourself.
- */
-
 import React from "react";
-
-/**
- * About background image
- *
- * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
- * represents what you see in that image.
- *
- * Need an image? Check out https://unsplash.com to download a image you
- * freely use on your site.
- */
 import image from "../images/motion-background.jpg";
 
 const imageAltText = "purple and blue abstract background";
 
-/**
- * Sort description that expands on your title on the Home component.
- */
 const description =
   "A passionate front-end developer with a love for building clean, functional, and visually engaging web applications.";
 
-/**
- * List of some of skills or technologies you work on, are learning,
- * passionate about, or enjoy,
- */
 const skillsList = [
   "HTML5",
   "CSS",
@@ -43,11 +19,6 @@ const skillsList = [
   
 ];
 
-/**
- * Use this to give more information about what you are passionate about,
- * how you best work, or even a quote. This will help someone learn more
- * about you on a professional level.
- */
 const detailOrQuote =
   "I'm a dedicated front-end developer who enjoys turning ideas into functional and visually appealing web applications. With experience in React, JavaScript, and modern web technologies, I focus on delivering clean, efficient solutions. I'm passionate about continuous learning and building web experiences that make a difference.";
 
